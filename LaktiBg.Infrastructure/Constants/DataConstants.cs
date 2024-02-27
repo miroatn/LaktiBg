bb472d91d1e405c6aa933bfa1192add3f103a446
miroatn <miro_atn@abv.bg>
2024-02-27 10:56:55 +0200

    Added DataConstants

---
+++ LaktiBg.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,35 @@
+﻿namespace LaktiBg.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public static class EventConstants
+        {
+            public const int NameMaxLenght = 100;
+            public const int NameMinLenght = 2;
+
+            public const int DescriptionMaxLenght = 700;
+            public const int DescriptionMinLenght = 5;
+        }
+
+        public static class PlaceConstants
+        {
+            public const int NameMaxLenght = 100;
+            public const int NameMinlenght = 2;
+
+            public const int RatingMaxValue = 7;
+            public const int RatingMinValue = 1;
+        }
+
+        public static class EventTypeConstants
+        {
+            public const int NameMaxLenght = 100;
+            public const int NameMinLenght = 3;
+        }
+
+        public static class CommentConstants
+        {
+            public const int TextMaxLenght = 200;
+            public const int TextMinLenght = 2;
+        }
+    }
+}
