5edff83d5c305e7766f5d48c6f811cce0de3adf5
miroatn <miro_atn@abv.bg>
2024-03-10 10:15:41 +0200

    Added event controller and viewmodel

---
+++ LaktiBg/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LaktiBg.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
