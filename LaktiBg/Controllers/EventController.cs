2108445f6432c14f42a799d079f7bd1958e276ef
miroatn <miro_atn@abv.bg>
2024-03-11 15:30:49 +0200

    Deleted duplicated data.

--- LaktiBg/Controllers/EventController.cs
+++ LaktiBg/Controllers/EventController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LaktiBg.Controllers
 {
+    [Authorize]
     public class EventController : Controller
     {
         public IActionResult Index()
