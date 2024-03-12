3d8d565bbcddac8c57baa8bfbc357dec6ff7438b
miroatn <miro_atn@abv.bg>
2024-03-12 21:00:51 +0200

    Added constants

---
+++ LaktiBg.Core/Constants/MessageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaktiBg.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "The {0} field is required.";
+
+        public const string LenghtMessage = "The {0} field must be between {2} and {1} characters long.";
+    }
+}
