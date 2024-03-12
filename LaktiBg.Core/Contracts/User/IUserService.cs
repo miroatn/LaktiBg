82c6d696fe2cb36542940cbb7dc7bc0e40c82a78
miroatn <miro_atn@abv.bg>
2024-03-12 20:58:26 +0200

    User claims and service added

---
+++ LaktiBg.Core/Contracts/User/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaktiBg.Core.Contracts.User
+{
+    public interface IUserService 
+    {
+        public Task<bool> ExistById(string userId);
+    }
+}
---
+++ LaktiBg.Core/Services/UserServices/UserService.cs
@@ -0,0 +1,28 @@
+﻿using LaktiBg.Core.Contracts.User;
+using LaktiBg.Infrastructure.Data.Common;
+using LaktiBg.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaktiBg.Core.Services.UserServices
+{
+ 
+    public class UserService : IUserService
+    {
+        private readonly IRepository repository;
+
+        public UserService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<bool> ExistById(string userId)
+        {
+            return await repository.AllReadOnly<ApplicationUser>()
+                .AnyAsync(u => u.Id == userId);
+        }
+    }
+}
