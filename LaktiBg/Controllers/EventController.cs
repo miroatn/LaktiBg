fb3ebd4fd5c385333022822125d3d49c67384c12
miroatn <miro_atn@abv.bg>
2024-03-12 20:57:52 +0200

    Added All method in event controller

--- LaktiBg/Controllers/EventController.cs
+++ LaktiBg/Controllers/EventController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using LaktiBg.Core.Contracts.Event;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaktiBg.Controllers
 {
     public class EventController : BaseController
     {
-        public IActionResult Index()
+        private readonly IEventService eventService;
+
+        public EventController(IEventService _eventService)
         {
-            return View();
+            eventService = _eventService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var model = await eventService.AllAsync();
+
+            return View(model);
         }
     }
 }
