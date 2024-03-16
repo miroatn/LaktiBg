2f8146b1f572f02519fbfd7a8498edefa1410102
miroatn <miro_atn@abv.bg>
2024-03-16 18:48:35 +0200

    Removed some unused usings

--- LaktiBg.Core/Contracts/Event/IEventService.cs
+++ LaktiBg.Core/Contracts/Event/IEventService.cs
@@ -1,14 +1,10 @@
 ﻿using LaktiBg.Core.Models.Event;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LaktiBg.Core.Contracts.Event
 {
     public interface IEventService
     {
         Task<IEnumerable<EventViewModel>> AllAsync();
+
     }
 }
--- LaktiBg.Core/Services/UserServices/UserService.cs
+++ LaktiBg.Core/Services/UserServices/UserService.cs
@@ -2,15 +2,10 @@
 using LaktiBg.Infrastructure.Data.Common;
 using LaktiBg.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LaktiBg.Core.Services.UserServices
 {
- 
+
     public class UserService : IUserService
     {
         private readonly IRepository repository;
