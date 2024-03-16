d1f1258aca9d0c1bc77f2d4fe3fcecf43d7d32f6
miroatn <miro_atn@abv.bg>
2024-03-16 18:46:13 +0200

    Added methods to the repository

--- LaktiBg.Infrastructure/Data/Common/IRepository.cs
+++ LaktiBg.Infrastructure/Data/Common/IRepository.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LaktiBg.Infrastructure.Data.Common
+﻿namespace LaktiBg.Infrastructure.Data.Common
 {
     public interface IRepository
     {
         IQueryable<T> All<T>() where T : class; 
 
         IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
+
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
 
     }
 }
--- LaktiBg.Infrastructure/Data/Common/Repository.cs
+++ LaktiBg.Infrastructure/Data/Common/Repository.cs
@@ -25,5 +25,20 @@ namespace LaktiBg.Infrastructure.Data.Common
             return DbSet<T>()
                 .AsNoTracking();
         }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
     }
 }
