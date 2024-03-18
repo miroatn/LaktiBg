3df0f57a3e0ad5c1d7b30af8e08efcdcecd611d1
miroatn <miro_atn@abv.bg>
2024-03-18 14:59:57 +0200

    Changed location of the logic in Place service

--- LaktiBg.Core/Services/PlaceServices/PlaceService.cs
+++ LaktiBg.Core/Services/PlaceServices/PlaceService.cs
@@ -1,4 +1,5 @@
 ﻿using LaktiBg.Core.Contracts.Place;
+using LaktiBg.Core.Models.Image;
 using LaktiBg.Core.Models.PlaceModels;
 using LaktiBg.Infrastructure.Data.Common;
 using LaktiBg.Infrastructure.Data.Models;
@@ -16,9 +17,42 @@ namespace LaktiBg.Core.Services.PlaceServices
 
         public async Task<int> CreateAsync(PlaceFormModel model, string ownerId)
         {
+            List<ImageViewModel> imageModels = new List<ImageViewModel>();
+
+            if (model.Files != null)
+            {
+                foreach (var file in model.Files)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(memoryStream);
+                        string fileExtention = Path.GetExtension(file.FileName.ToLower());
+
+                        if (memoryStream.Length < 4194304 && isPhoto(fileExtention))
+                        {
+                            var newphoto = new ImageViewModel()
+                            {
+                                Bytes = memoryStream.ToArray(),
+                                Description = file.FileName,
+                                FileExtension = Path.GetExtension(file.FileName),
+                                Size = file.Length,
+                            };
+
+                            imageModels.Add(newphoto);
+                        }
+                        else
+                        {
+                            //ModelState.AddModelError("File", "The file is too large.");
+                        }
+                    }
+
+                }
+
+            }
+
             List<Image> images = new List<Image>();
 
-            foreach (var item in model.Images)
+            foreach (var item in imageModels)
             {
                 var image = new Image()
                 {
@@ -47,6 +81,14 @@ namespace LaktiBg.Core.Services.PlaceServices
             await repository.SaveChangesAsync();
            
             return place.Id;
+        }
+
+        private bool isPhoto(string fileExtention)
+        {
+            return fileExtention == ".jpg" 
+                || fileExtention == ".jpeg" 
+                || fileExtention == ".png" 
+                || fileExtention == ".bmp";
         }
 
         public Task<int> CreateAsync(PlaceFormModel model)
--- LaktiBg/Controllers/PlaceController.cs
+++ LaktiBg/Controllers/PlaceController.cs
@@ -29,53 +29,19 @@ namespace LaktiBg.Controllers
 
         public async Task<IActionResult> Add(PlaceFormModel model)
         {
+
+            string userId = User.Id();
+
+            int newPlaceId = await placeService.CreateAsync(model, userId);
+
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
-            List<ImageViewModel> images = new List<ImageViewModel>();
-
-            if (model.Files != null)
-            {
-                foreach (var file in model.Files)
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        await file.CopyToAsync(memoryStream);
-                        // Upload the file if less than 4 MB  
-                        if (memoryStream.Length < 4194304)
-                        {
-                            //based on the upload file to create Photo instance.  
-                            //You can also check the database, whether the image exists in the database.  
-                            var newphoto = new ImageViewModel()
-                            {
-                                Bytes = memoryStream.ToArray(),
-                                Description = file.FileName,
-                                FileExtension = Path.GetExtension(file.FileName),
-                                Size = file.Length,
-                            };
-                            //add the photo instance to the list.  
-                            images.Add(newphoto);
-                        }
-                        else
-                        {
-                            ModelState.AddModelError("File", "The file is too large.");
-                        }
-                    }
-
-                }
-
-            }
-
-
-            model.Images = images;
-            string userId = User.Id();
-
-            int newPlaceId = await placeService.CreateAsync(model, userId);
-
             return RedirectToAction(nameof(Details), new {id = newPlaceId});
         }
+
 
         [HttpGet]
 
