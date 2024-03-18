a7ff555dc665e03714a5cd72500ba44e25730b9a
miroatn <miro_atn@abv.bg>
2024-03-18 16:18:43 +0200

    Added image service to implement image size reduction.

---
+++ LaktiBg.Core/Services/ImageServices/ImageService.cs
@@ -0,0 +1,28 @@
+﻿using LaktiBg.Core.Contracts.Image;
+using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaktiBg.Core.Services.ImageServices
+{
+    public class ImageService : IImageService
+    {
+        public async Task CompressAndSaveImageAsync(IFormFile imageFile, string outputPath, int quality)
+        {
+            using var imageStream = imageFile.OpenReadStream();
+            using var image = Image.Load(imageStream);
+            var encoder = new JpegEncoder
+            {
+                Quality = quality, // Adjust this value for desired compression quality
+            };
+
+            await Task.Run(() => image.Save(outputPath, encoder));
+
+        }
+    }
+}
