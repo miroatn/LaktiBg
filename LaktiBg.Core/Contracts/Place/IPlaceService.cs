558054d1f6968b16f72f4447115dfd1f654dc019
miroatn <miro_atn@abv.bg>
2024-03-18 19:48:31 +0200

    Added All and Details methods

--- LaktiBg.Core/Contracts/Place/IPlaceService.cs
+++ LaktiBg.Core/Contracts/Place/IPlaceService.cs
@@ -5,5 +5,9 @@ namespace LaktiBg.Core.Contracts.Place
     public interface IPlaceService
     {
         Task<int> CreateAsync(PlaceFormModel model, string ownerId);
+
+        Task<IEnumerable<PlaceViewModel>> AllAsync();
+
+        Task<PlaceViewModel> Details(int id);
     }
 }
--- LaktiBg.Core/Services/PlaceServices/PlaceService.cs
+++ LaktiBg.Core/Services/PlaceServices/PlaceService.cs
@@ -5,6 +5,8 @@ using LaktiBg.Core.Services.ImageServices;
 using LaktiBg.Infrastructure.Data.Common;
 using LaktiBg.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LaktiBg.Core.Services.PlaceServices
 {
@@ -15,6 +17,43 @@ namespace LaktiBg.Core.Services.PlaceServices
         public PlaceService(IRepository _repository)
         {
             repository = _repository;
+        }
+
+        public async Task<IEnumerable<PlaceViewModel>> AllAsync()
+        {
+            List<PlaceViewModel> models = await repository.All<Place>().Select(p => new PlaceViewModel
+            {
+                Id = p.Id,
+                IsPublic = p.IsPublic,
+                Name = p.Name,
+                OwnerId = p.OwnerId,
+                Contact = p.Contact,
+                Address = p.Address,
+                Images = p.Images.Select(i => new ImageViewModel 
+                {
+                    Id = i.Id,
+                    Bytes = i.Bytes,
+                    Description = i.Description,
+                    FileExtension = i.FileExtension,
+                    Size = i.Size,
+                    PlaceId = i.PlaceId,
+                }).ToList(),
+                Rating = p.Rating,
+            }).ToListAsync();
+
+            List<byte[]> imageBytesList = new List<byte[]>();
+
+            foreach (PlaceViewModel model in models)
+            {
+                foreach (var image in model.Images)
+                {
+                    string base64String = Convert.ToBase64String(image.Bytes);
+                    string imageDataURL = $"data:image/png;base64,{base64String}";
+                    model.ImagesToShow.Add(imageDataURL);
+                }
+            }
+
+            return models;
         }
 
         public async Task<int> CreateAsync(PlaceFormModel model, string ownerId)
@@ -96,10 +135,42 @@ namespace LaktiBg.Core.Services.PlaceServices
             return place.Id;
         }
 
-
-        public Task<int> CreateAsync(PlaceFormModel model)
+        public async Task<PlaceViewModel> Details(int id)
         {
-            throw new NotImplementedException();
+            Place? place = await repository.GetByIdAsync<Place>(id);
+
+            PlaceViewModel model = new();
+            model.Images = await repository.All<Image>().Where(x => x.PlaceId == id).Select(i => new ImageViewModel
+            {
+                Id = i.Id,
+                Bytes = i.Bytes,
+                Description = i.Description,
+                FileExtension = i.FileExtension,
+                Size = i.Size,
+                PlaceId = i.PlaceId,
+            }).ToListAsync();
+
+            if (place != null)
+            {
+                model.Id = place.Id;
+                model.Name = place.Name;
+                model.Contact = place.Contact;
+                model.Address = place.Address;
+                model.IsPublic = place.IsPublic;
+                model.Rating = 5;
+
+                List<byte[]> imageBytesList = new List<byte[]>();
+
+                foreach (var image in model.Images)
+                {
+                    string base64String = Convert.ToBase64String(image.Bytes);
+                    string imageDataURL = $"data:image/png;base64,{base64String}";
+                    model.ImagesToShow.Add(imageDataURL);
+                }
+
+            }
+
+            return model;
         }
 
         private bool isPhoto(string fileExtention)
