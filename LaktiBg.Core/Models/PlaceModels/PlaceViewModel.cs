413c8d58c338a9fe1f1762cfca8244bcf677c75f
miroatn <miro_atn@abv.bg>
2024-03-18 19:48:46 +0200

    PlaceViewModel changed

---
+++ LaktiBg.Core/Models/PlaceModels/PlaceViewModel.cs
@@ -0,0 +1,25 @@
+﻿using LaktiBg.Core.Models.Image;
+
+namespace LaktiBg.Core.Models.PlaceModels
+{
+    public class PlaceViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? OwnerId { get; set; }
+
+        public string? Contact { get; set; }
+
+        public string? Address { get; set; }
+
+        public bool IsPublic { get; set; } = false;
+
+        public ICollection<ImageViewModel> Images { get; set; } = new List<ImageViewModel>();
+
+        public List<string> ImagesToShow { get; set; } = new List<string>();
+
+        public decimal Rating { get; set; }
+    }
+}
