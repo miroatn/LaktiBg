688a25ecabaa65dbc72ff4b8ad4dd80fd9ffa929
miroatn <miro_atn@abv.bg>
2024-03-23 14:31:34 +0200

    Added msg for NoRestriction

--- LaktiBg.Core/Constants/MessageConstants.cs
+++ LaktiBg.Core/Constants/MessageConstants.cs
@@ -13,5 +13,7 @@ namespace LaktiBg.Core.Constants
         public const string LenghtMessage = "The {0} field must be between {2} and {1} characters long.";
 
         public const string RatingRange = "The rating must be between {0} and {1}";
+
+        public const string NoRestrictionAdded = "Няма зададено ограничение";
     }
 }
