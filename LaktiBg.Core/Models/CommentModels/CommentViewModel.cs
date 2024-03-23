3b0b672b9ccf15ee2fc1f067797c06b02dae0413
miroatn <miro_atn@abv.bg>
2024-03-23 09:24:51 +0200

    Added commentview model

---
+++ LaktiBg.Core/Models/CommentModels/CommentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LaktiBg.Core.Models.CommentModels
+{
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string AuthorId { get; set; } = string.Empty;
+        public int EventId { get; set; }
+    }
+}
