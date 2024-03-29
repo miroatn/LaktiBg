ed8a94da7fe1edc3dede5d69f9bc923b92400cba
miroatn <miro_atn@abv.bg>
2024-03-29 16:04:20 +0200

    Added method for ViewProfile

--- LaktiBg.Core/Services/UserServices/UserService.cs
+++ LaktiBg.Core/Services/UserServices/UserService.cs
@@ -1,4 +1,6 @@
-﻿using LaktiBg.Core.Contracts.User;
+﻿using LaktiBg.Core.Contracts.Event;
+using LaktiBg.Core.Contracts.User;
+using LaktiBg.Core.Models.UserModels;
 using LaktiBg.Infrastructure.Data.Common;
 using LaktiBg.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +11,7 @@ namespace LaktiBg.Core.Services.UserServices
     public class UserService : IUserService
     {
         private readonly IRepository repository;
+
 
         public UserService(IRepository _repository)
         {
@@ -45,6 +48,66 @@ namespace LaktiBg.Core.Services.UserServices
                 .Select(u => u.Rating)
                 .FirstOrDefaultAsync();
 
+        }
+
+        public async Task<UserViewModel> GetUserViewModelByIdAsync(string userId)
+        {
+            UserViewModel? model = await repository.AllReadOnly<ApplicationUser>()
+                                                    .Where(au => au.Id == userId)
+                                                    .Select(au => new UserViewModel
+                                                    {
+                                                        Id = au.Id,
+                                                        FirstName = au.FirstName,
+                                                        LastName = au.LastName,
+                                                        BirthDate = au.BirthDate,
+                                                        Rating = au.Rating,
+                                                        Friends = au.Friends.Select(f => new UserFriendsViewModel
+                                                        {
+                                                            UserId = f.UserId,
+                                                            UserFriendId = f.UserFriendId
+                                                        }).ToList(),
+                                                        RegistrationDate = au.RegistrationDate,
+                                                        Address = au.Address == null ? "No information" : au.Address,
+                                                        Description = au.Description == null ? "No information" : au.Description,
+                                                        UserName = au.UserName,
+                                                        PhoneNumber = au.PhoneNumber == null ? "No information" : au.PhoneNumber,
+                                                    })
+                                                    .FirstOrDefaultAsync();
+
+            if (model != null)
+            {
+                model.FinishedEvents = await GetUsersFinishedEventsAsync(userId);
+
+                model.OngoingEvents = await GetUsersOnGoingEventsAsync(userId);
+            }
+
+
+            return model;
+        }
+
+        public async Task<ICollection<UsersEventsViewModel>> GetUsersFinishedEventsAsync(string userId)
+        {
+            return await repository.AllReadOnly<UsersEvents>()
+                                                    .Where(ue => ue.UserId == userId &&
+                                                    ue.Event.IsFinished == true)
+                                                    .Select(ue => new UsersEventsViewModel
+                                                    {
+                                                        UserId = ue.UserId,
+                                                        EventId = ue.Event.Id,
+                                                    }).ToListAsync();
+        }
+
+
+        public async Task<ICollection<UsersEventsViewModel>> GetUsersOnGoingEventsAsync(string userId)
+        {
+            return await repository.AllReadOnly<UsersEvents>()
+                                                    .Where(ue => ue.UserId == userId &&
+                                                    ue.Event.IsFinished == false)
+                                                    .Select(ue => new UsersEventsViewModel
+                                                    {
+                                                        UserId = ue.UserId,
+                                                        EventId = ue.Event.Id,
+                                                    }).ToListAsync();
         }
     }
 }
