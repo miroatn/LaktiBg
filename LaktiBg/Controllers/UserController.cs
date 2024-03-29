e11888f7fda8221c03a220820d224e12a813fe5f
miroatn <miro_atn@abv.bg>
2024-03-29 16:04:45 +0200

    Added method for ViewProfile

---
+++ LaktiBg/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using LaktiBg.Core.Contracts.User;
+using LaktiBg.Core.Models.UserModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaktiBg.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+        public async Task<IActionResult> ViewProfile(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (await userService.ExistById(id) == false)
+            {
+                return BadRequest();
+            }
+
+            UserViewModel model = await userService.GetUserViewModelByIdAsync(id);
+
+            return View(model);
+        }
+    }
+}
