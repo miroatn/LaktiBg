4d117627f5547d7fc54e0f76925a22157d832682
miroatn <miro_atn@abv.bg>
2024-04-02 16:25:29 +0300

    Admin area added

--- LaktiBg/Program.cs
+++ LaktiBg/Program.cs
@@ -35,10 +35,20 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapRazorPages();
+
+});
+
 
 await app.CreateAdminRoleAsync();
 
