79c406347d3678f918b3be5baa8dd8cdbbee2544
miroatn <miro_atn@abv.bg>
2024-04-03 11:33:07 +0300

    Added new exception

---
+++ LaktiBg.Core/Exceptions/UnauthorizedActionException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaktiBg.Core.Exceptions
+{
+    public class UnauthorizedActionException : Exception
+    {
+        public UnauthorizedActionException()
+        {
+            
+        }
+
+        public UnauthorizedActionException(string message)
+            :base(message)
+        {
+            
+        }
+    }
+}
