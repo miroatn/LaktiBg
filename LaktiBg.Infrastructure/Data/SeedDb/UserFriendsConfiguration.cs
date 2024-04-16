333deda25abb8696034b5041b1b96cafd4fd32a4
miroatn <miro_atn@abv.bg>
2024-04-16 18:42:55 +0300

    Seeded UserFriends to the DB

---
+++ LaktiBg.Infrastructure/Data/SeedDb/UserFriendsConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using LaktiBg.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LaktiBg.Infrastructure.Data.SeedDb
+{
+    public class UserFriendsConfiguration : IEntityTypeConfiguration<UserFriends>
+    {
+        public void Configure(EntityTypeBuilder<UserFriends> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new UserFriends[] { data.UserFriends, data.FriendUserFriends });
+        }
+    }
+}
