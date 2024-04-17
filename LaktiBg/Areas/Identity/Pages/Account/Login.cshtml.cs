e3315c1a5b30277efa7d7665e0e748b8a722f073
miroatn <miro_atn@abv.bg>
2024-04-17 19:53:17 +0300

    MInor fixes for login page

--- LaktiBg/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ LaktiBg/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -129,7 +129,7 @@ namespace LaktiBg.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Невалидно потребителско име или парола.");
                     return Page();
                 }
             }
